f567ac0d43209825168a0da5c1e7a86b395852c7
Shivam Pandey <146947369+ShivamPandey45@users.noreply.github.com>
2024-09-06 17:59:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+var len=document.getElementsByClassName("drum").length;
+for(var i=0;i<len;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var word=this.innerHTML;
+        makeSound(word);
+       
+    })
+}
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+})
+function makeSound(lis){
+    switch(lis){
+        case "w":
+            var audio=new Audio("sounds/tom-1.mp3")
+            audio.play();
+         break;
+         case "a":
+            var audio3=new Audio("sounds/tom-3.mp3")
+            audio3.play();
+          break;
+          case "s":
+            var audio4=new Audio("sounds/tom-4.mp3")
+            audio4.play();
+        break;  
+        case "d":
+            var audio4=new Audio("sounds/snare.mp3")
+            audio4.play();
+        break;  
+        case "j":
+            var audio4=new Audio("sounds/kick-bass.mp3")
+            audio4.play();
+        break;
+        case "k":
+            var audio4=new Audio("sounds/crash.mp3")
+            audio4.play();
+        break;    
+        
+
+        
+
+        default:
+            var audio2=new Audio("sounds/tom-2.mp3") 
+            audio2.play();   
+           
+
+    }
+}
